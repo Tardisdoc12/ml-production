f4e7fbc4410734dd02ce85aea560d56b4f142c6f
Jean <94552702+Tardisdoc12@users.noreply.github.com>
2025-05-12 11:39:25 +0200

    #2 : add des libraries à utiliser

---
+++ src/main.py
@@ -0,0 +1,22 @@
+################################################################################
+# filename: main.py
+# Author: Jean Anquetil
+# Email: janquetil@e-vitech.com
+# Date: 12/05,2025
+################################################################################
+
+import numpy as np
+from datasets import load_dataset
+from sklearn.metrics import accuracy_score, f1_score
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    Trainer,
+    TrainingArguments,
+)
+
+################################################################################
+
+################################################################################
+# End of File
+################################################################################
