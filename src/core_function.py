c574a73e14dd483d6ca45ae85b7586ce356df8e6
Jean <94552702+Tardisdoc12@users.noreply.github.com>
2025-05-16 16:10:50 +0200

    #2 correction d'un if

--- src/core_function.py
+++ src/core_function.py
@@ -32,7 +32,7 @@ def get_model(model_name: str, from_output_dir=None) -> tuple:
     """
     Return tokenizer and model
     """
-    if not from_output_dir is None:
+    if from_output_dir is not None:
         model_name = from_output_dir
     tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
     model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
