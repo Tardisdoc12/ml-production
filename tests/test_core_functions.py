cefd83afb8e5a154f6deea3b0961c20450757096
Jean <94552702+Tardisdoc12@users.noreply.github.com>
2025-05-16 16:23:25 +0200

    Update test_core_functions.py

--- tests/test_core_functions.py
+++ tests/test_core_functions.py
@@ -1,10 +1,8 @@
 import sys
 import os
-
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
 )
-
 from core_function import compute_metrics
 import numpy as np
 
