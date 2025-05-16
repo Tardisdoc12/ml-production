6dabfa0917039644f63b6463a89c06bb7844ebec
Jean <94552702+Tardisdoc12@users.noreply.github.com>
2025-05-16 16:20:33 +0200

    Update test_core_functions.py

--- tests/test_core_functions.py
+++ tests/test_core_functions.py
@@ -1,9 +1,21 @@
-from src.core_function import compute_metrics
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
+from core_function import compute_metrics
+import numpy as np
 
 def test_compute_metrics():
-    fake_data = ([0, 1, 0, 2, 3], [0, 2, 0, 1, 3])
+    fake_pred = np.array([
+        [0,0.7],
+        [1,0.8],
+        [0,0.71],
+        [1,0.86],
+        [0,0.73]
+    ])
+    fake_data = (fake_pred, [0, 1, 1, 0, 0])
     results = compute_metrics(fake_data)
-    print(results)
     assert "f1" in results.keys()
-    assert results["f1"] == 3 / 5
+    assert results["f1"] == 0.75
+
+test_compute_metrics()
